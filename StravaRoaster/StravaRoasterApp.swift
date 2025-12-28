88c60a674a344ae973b95be1f2c0a19b19803d8b
rhblitstein <rhblitstein@gmail.com>
2025-12-28 12:58:12 -0700

    Initial Commit

---
+++ StravaRoaster/StravaRoasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravaRoasterApp.swift
+//  StravaRoaster
+//
+//  Created by Rebecca Blitstein on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct StravaRoasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
